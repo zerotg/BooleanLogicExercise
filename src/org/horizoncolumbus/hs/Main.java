61552e52dcf75a55084b78f7f12e1940d084577d
misa <student.m.bellohernandez63@horizoncolumbus.org>
2020-09-25 09:44:06 -0400

    Initial commit

---
+++ src/org/horizoncolumbus/hs/Main.java
@@ -0,0 +1,23 @@
+package org.horizoncolumbus.hs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean Junior = false;
+        boolean Senior = false;
+        boolean CTE = false;
+        boolean Teacher = false;
+        boolean admin = true;
+        boolean home =  true;
+
+        if(Junior || Senior && CTE){
+            System.out.println("You get windows laptop");
+        }
+        else if (Teacher || admin && home){
+            System.out.println("you get macbook");
+        }
+        else{
+            System.out.println("chromebook for you");
+        }
+    }
+}
